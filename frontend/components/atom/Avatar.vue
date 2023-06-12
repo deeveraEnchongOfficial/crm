023ad65a0f0ac19d243754b324ca361352e64865
Enchong <deevera.enchong18@gmail.com>
2023-06-12 11:39:38 +0800

    updated logo

--- frontend/components/atom/Avatar.vue
+++ frontend/components/atom/Avatar.vue
@@ -4,7 +4,7 @@
 <template>
   <header class="flex items-center">
     <slot name="avatar">
-      <img id="avatar" src="~/assets/images/prodash.png" width="32px" height="32px" class="object-cover w-8 h-8 rounded" />
+      <img id="avatar" src="~/assets/images/prodash.jpg" width="32px" height="32px" class="object-cover w-8 h-8 rounded" />
     </slot>
   </header>
 </template>
--- frontend/components/atom/Logo.vue
+++ frontend/components/atom/Logo.vue
@@ -6,10 +6,10 @@
     <slot name="logo">
       <img
         id="logo"
-        src="~/assets/images/prodash.png"
+        src="~/assets/images/prodash.jpg"
         width="32px"
         height="32px"
-        class="object-cover w-8 h-8 rounded-full"
+        class="object-cover w-10 h-10 rounded-full"
       />
     </slot>
   </header>
