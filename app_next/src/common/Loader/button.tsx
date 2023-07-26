3d0f602bc6393a023565853e9392d8bda1031322
Auriel James <aurieljames11@gmail.com>
2023-07-26 19:57:05 +0800

    issue/#62 Button loader implemented

---
+++ app_next/src/common/Loader/button.tsx
@@ -0,0 +1,8 @@
+const ButtonLoader = () => {
+    return(
+        <div className="h-6 w-6 animate-spin rounded-full border-2 ml-auto mr-auto border-white border-t-transparent"></div>
+    );
+  };
+  
+  export default ButtonLoader;
+  
