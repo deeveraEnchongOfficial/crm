afa8413f4ea4d1debc816d714e4300af3fd7ba38
Auriel James Fernandez <61530799+chaesthetics@users.noreply.github.com>
2023-08-04 07:35:17 +0800

    Update DefaultAvatar.tsx
    Added output value for 2 initials

--- app_next/src/components/DefaultAvatar.tsx
+++ app_next/src/components/DefaultAvatar.tsx
@@ -7,11 +7,12 @@ const DefaultAvatar: React.FC<AvatarProps> = ({firstName, lastName}) => {
 
     const firstNameInitial = firstName.charAt(0);
     const lastNameInitial = lastName.charAt(0);
+    const initials = firstNameInitial+lastNameInitial;
     
 
     return(
         <div className="relative inline-flex items-center justify-center w-10 h-10 overflow-hidden bg-meta-9 rounded-full dark:bg-gray-600">
-            <span className="font-medium text-gray-600 dark:text-gray-300">{firstNameInitial+lastNameInitial}</span>
+            <span className="font-medium text-gray-600 dark:text-gray-300">{initials}</span>
         </div>
     )
 };
