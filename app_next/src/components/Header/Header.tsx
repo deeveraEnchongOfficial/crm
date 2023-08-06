c4ebc430ed368eb272a1c24f3f74a7c10f982e19
deeveraEnchongOfficial <deveraflorencio18@gmail.com>
2023-08-06 13:03:12 +0800

    major changes

--- app_next/src/components/Header/Header.tsx
+++ app_next/src/components/Header/Header.tsx
@@ -1,10 +1,11 @@
-import Link from 'next/link';
-import Image from 'next/image';
-import up_next from '@/images/logo/up_next.png';
-import DarkModeSwitcher from '../DarkModeSwitcher';
-import DropdownMessage from '@/components/Dropdowns/DropdownMessage';
-import DropdownNotification from '@/components/Dropdowns/DropdownNotification';
-import DropdownUser from '@/components/Dropdowns/DropdownUser';
+import Link from "next/link";
+import Image from "next/image";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import up_next from "@/images/logo/up_next.png";
+import DarkModeSwitcher from "../DarkModeSwitcher";
+import DropdownMessage from "@/components/Dropdowns/DropdownMessage";
+import DropdownNotification from "@/components/Dropdowns/DropdownNotification";
+import DropdownUser from "@/components/Dropdowns/DropdownUser";
 
 const Header = (props: {
   sidebarOpen: string | boolean | undefined;
@@ -27,29 +28,29 @@ const Header = (props: {
               <span className="absolute right-0 w-full h-full du-block">
                 <span
                   className={`relative top-0 left-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-[0] duration-200 ease-in-out dark:bg-white ${
-                    !props.sidebarOpen && '!w-full delay-300'
+                    !props.sidebarOpen && "!w-full delay-300"
                   }`}
                 ></span>
                 <span
                   className={`relative top-0 left-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-150 duration-200 ease-in-out dark:bg-white ${
-                    !props.sidebarOpen && 'delay-400 !w-full'
+                    !props.sidebarOpen && "delay-400 !w-full"
                   }`}
                 ></span>
                 <span
                   className={`relative top-0 left-0 my-1 block h-0.5 w-0 rounded-sm bg-black delay-200 duration-200 ease-in-out dark:bg-white ${
-                    !props.sidebarOpen && '!w-full delay-500'
+                    !props.sidebarOpen && "!w-full delay-500"
                   }`}
                 ></span>
               </span>
               <span className="absolute right-0 w-full h-full rotate-45">
                 <span
                   className={`absolute left-2.5 top-0 block h-full w-0.5 rounded-sm bg-black delay-300 duration-200 ease-in-out dark:bg-white ${
-                    !props.sidebarOpen && '!h-0 !delay-[0]'
+                    !props.sidebarOpen && "!h-0 !delay-[0]"
                   }`}
                 ></span>
                 <span
                   className={`delay-400 absolute left-0 top-2.5 block h-0.5 w-full rounded-sm bg-black duration-200 ease-in-out dark:bg-white ${
-                    !props.sidebarOpen && '!h-0 !delay-200'
+                    !props.sidebarOpen && "!h-0 !delay-200"
                   }`}
                 ></span>
               </span>
@@ -58,7 +59,7 @@ const Header = (props: {
           {/* <!-- Hamburger Toggle BTN --> */}
 
           <Link className="flex-shrink-0 block lg:hidden" href="/">
-            <Image src={up_next} alt="up_next" width={80} height={80}/>
+            <Image src={up_next} alt="up_next" width={80} height={80} />
           </Link>
         </div>
 
@@ -66,27 +67,7 @@ const Header = (props: {
           <form action="https://formbold.com/s/unique_form_id" method="POST">
             <div className="relative">
               <button className="absolute left-0 -translate-y-1/2 top-1/2">
-                <svg
-                  className="fill-body hover:fill-primary dark:fill-bodydark dark:hover:fill-primary"
-                  width="20"
-                  height="20"
-                  viewBox="0 0 20 20"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    clipRule="evenodd"
-                    d="M9.16666 3.33332C5.945 3.33332 3.33332 5.945 3.33332 9.16666C3.33332 12.3883 5.945 15 9.16666 15C12.3883 15 15 12.3883 15 9.16666C15 5.945 12.3883 3.33332 9.16666 3.33332ZM1.66666 9.16666C1.66666 5.02452 5.02452 1.66666 9.16666 1.66666C13.3088 1.66666 16.6667 5.02452 16.6667 9.16666C16.6667 13.3088 13.3088 16.6667 9.16666 16.6667C5.02452 16.6667 1.66666 13.3088 1.66666 9.16666Z"
-                    fill=""
-                  />
-                  <path
-                    fillRule="evenodd"
-                    clipRule="evenodd"
-                    d="M13.2857 13.2857C13.6112 12.9603 14.1388 12.9603 14.4642 13.2857L18.0892 16.9107C18.4147 17.2362 18.4147 17.7638 18.0892 18.0892C17.7638 18.4147 17.2362 18.4147 16.9107 18.0892L13.2857 14.4642C12.9603 14.1388 12.9603 13.6112 13.2857 13.2857Z"
-                    fill=""
-                  />
-                </svg>
+                <MagnifyingGlassIcon class="h-6 w-6 text-gray-500" />
               </button>
 
               <input
